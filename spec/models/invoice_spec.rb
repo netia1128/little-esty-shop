c47c89498d6aefda937c968ec24019a77bbdf116
netia1128 <netia1128@hotmail.com>
2021-06-14 14:59:56 -0600

    Add test and code for invoice #discounted/undiscounted revenue

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -2,17 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Invoice do
   before(:each) do
-    @merchant = Merchant.create!(name: 'Sally Handmade')
-    @merchant_2 = Merchant.create!(name: 'Billy Mandmade')
-    @item =  @merchant.items.create!(name: 'Qui Essie', description: 'Lorem ipsim', unit_price: 75107)
-    @item_2 =  @merchant.items.create!(name: 'Essie', description: 'Lorem ipsim', unit_price: 75107)
-    @item_3 = @merchant_2.items.create!(name: 'Glowfish Markdown', description: 'Lorem ipsim', unit_price: 55542)
-    @customer = Customer.create!(first_name: 'Joey', last_name: 'Ondricka') 
-    @invoice = Invoice.create!(customer_id: @customer.id, status: 'completed')
-    @invoice_2 = Invoice.create!(customer_id: @customer.id, status: 'completed')
-    InvoiceItem.create!(item_id: @item.id, invoice_id: @invoice.id, quantity: 539, unit_price: 13635, status: 1)
-    InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice.id, quantity: 539, unit_price: 13635, status: 1)
-    InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice.id, quantity: 539, unit_price: 13635, status: 1)
+    
   end
 
   describe 'relationships' do
@@ -20,28 +10,36 @@ RSpec.describe Invoice do
     it {should have_many :invoice_items}
     it {should have_many :transactions}
     it {should have_many(:items).through(:invoice_items)}
+    it {should have_many(:merchants).through(:items)}
   end
 
   describe 'class methods' do
     describe '#filter_by_unshipped_order_by_age' do
       it 'returns all invoices with unshipped items sorted by creation date' do
-        expect(Invoice.filter_by_unshipped_order_by_age.count("distinct invoices.id")).to eq(45)
+        expect(Invoice.filter_by_unshipped_order_by_age.count("distinct invoices.id")).to eq(44)
         expect(Invoice.filter_by_unshipped_order_by_age.first.id).to eq(10)
       end
     end
   end
 
   describe 'instance methods' do
-    # Test pulls instance from test db to test against
-    it 'has array of available status options' do
-      single_invoice = Invoice.last
-      expect(single_invoice.statuses).to eq ['in progress', 'completed', 'cancelled']
+    describe '#stauses' do
+      it 'has array of available status options' do
+        single_invoice = Invoice.last
+        expect(single_invoice.statuses).to eq ['in progress', 'completed', 'cancelled']
+      end
     end
-
-    it 'calculates total potential revenue' do
-      single_invoice = Invoice.last
-      total_revenue = single_invoice.invoice_items.total_revenue # utilizes class method from InvoiceItems
-      expect(single_invoice.revenue).to eq total_revenue
+    describe '#undiscounted_revenue' do
+      it 'calculates total undiscounted revenue' do
+        invoice = Invoice.find(29)
+        expect(invoice.undiscounted_revenue).to eq(1281794)
+      end
+    end
+    describe '#discounted_revenue' do
+      it 'calculates total discounted revenue' do
+        invoice = Invoice.find(29)
+        expect(invoice.discounted_revenue).to eq(1233316.6)
+      end
     end
   end
 end
